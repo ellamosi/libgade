a1a7070ace62979b0bc56646f6ab863642c15e23
Eduard Llamosí <eduard@quri.com>
2017-10-25 20:23:18 -0700

    Add individual CPU instruction tests

---
+++ testsuite/tests/cpu_instrs_bit_ops/src/tc_cpu_instrs_bit_ops.adb
@@ -0,0 +1,6 @@
+with Test_LCD_Output; use Test_LCD_Output;
+
+procedure TC_CPU_Instrs_Bit_Ops is
+begin
+   Run_LCD_Test ("10-bit ops.gb", "CPU Instructions: Bit Ops", 900);
+end TC_CPU_Instrs_Bit_Ops;
---
+++ testsuite/tests/cpu_instrs_interrupts/src/tc_cpu_instrs_interrupts.adb
@@ -0,0 +1,6 @@
+with Test_LCD_Output; use Test_LCD_Output;
+
+procedure TC_CPU_Instrs_Interrupts is
+begin
+   Run_LCD_Test ("02-interrupts.gb", "CPU Instructions: Interrupts", 65);
+end TC_CPU_Instrs_Interrupts;
