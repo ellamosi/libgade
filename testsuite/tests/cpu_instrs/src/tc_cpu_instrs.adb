c005cc42da662c993f91bba586fe4e2b4d1651f2
Eduard Llamosí <eduard@quri.com>
2017-10-25 19:49:59 -0700

    Enable style and code cleanup compiler switches

--- testsuite/tests/cpu_instrs/src/tc_cpu_instrs.adb
+++ testsuite/tests/cpu_instrs/src/tc_cpu_instrs.adb
@@ -2,5 +2,5 @@ with Test_LCD_Output; use Test_LCD_Output;
 
 procedure TC_CPU_Instrs is
 begin
-   Run_LCD_Test("cpu_instrs.gb", "CPU instructions", 3250);
+   Run_LCD_Test ("cpu_instrs.gb", "CPU instructions", 3250);
 end TC_CPU_Instrs;
--- testsuite/utils/src/test_lcd_output.adb
+++ testsuite/utils/src/test_lcd_output.adb
@@ -12,6 +12,16 @@ with Gade.Video_Buffer; use Gade.Video_Buffer;
 
 package body Test_LCD_Output is
 
+   function Allocate_Bitmap return not null Any_Bitmap_Buffer;
+   procedure Copy_Buffer
+     (From : RGB32_Display_Buffer;
+      To   : not null Any_Bitmap_Buffer);
+   procedure Run_ROM
+     (ROM_File : String;
+      Buffer   : access RGB32_Display_Buffer;
+      Frames   : Positive);
+   procedure Write_LCD_Output (Buffer : RGB32_Display_Buffer);
+
    ---------------------
    -- Allocate_Bitmap --
    ---------------------
@@ -43,10 +53,10 @@ package body Test_LCD_Output is
    begin
       for Y in 0 .. Display_Height - 1 loop
          for X in 0 .. Display_Width - 1 loop
-            Pix.Red   := UInt8(From(Y, X).Red);
-            Pix.Green := UInt8(From(Y, X).Green);
-            Pix.Blue  := UInt8(From(Y, X).Blue);
-            To.Set_Pixel((X, Y), Pix);
+            Pix.Red   := UInt8 (From (Y, X).Red);
+            Pix.Green := UInt8 (From (Y, X).Green);
+            Pix.Blue  := UInt8 (From (Y, X).Blue);
+            To.Set_Pixel ((X, Y), Pix);
          end loop;
       end loop;
    end Copy_Buffer;
@@ -62,10 +72,10 @@ package body Test_LCD_Output is
    is
       G : Gade_Type;
    begin
-      Create(G);
-      Load_ROM(G, Test_Dir & '/' & ROM_File);
-      for i in 1..Frames loop
-         Next_Frame(G, Buffer);
+      Create (G);
+      Load_ROM (G, Test_Dir & '/' & ROM_File);
+      for i in 1 .. Frames loop
+         Next_Frame (G, Buffer);
       end loop;
    end Run_ROM;
 
@@ -77,7 +87,7 @@ package body Test_LCD_Output is
       Output_File : Ada.Streams.Stream_IO.File_Type;
       BM          : constant not null Any_Bitmap_Buffer := Allocate_Bitmap;
    begin
-      Copy_Buffer(Buffer, BM);
+      Copy_Buffer (Buffer, BM);
       Create (Output_File, Out_File, Test_Dir & "/test.bmp");
       Write_BMP_File (Output_File, BM.all);
       Close (Output_File);
@@ -95,11 +105,11 @@ package body Test_LCD_Output is
       Buffer   : aliased RGB32_Display_Buffer;
    begin
       Run_ROM (ROM_File, Buffer'Access, Frames);
-      Write_LCD_Output(Buffer);
+      Write_LCD_Output (Buffer);
       if Binnary_Equal (Test_Dir & "/test.bmp", Test_Dir & "/ref.bmp") then
-         Put_Line(Test_Name & " test OK");
+         Put_Line (Test_Name & " test OK");
       else
-         Put_Line(Test_Name & " test FAILED");
+         Put_Line (Test_Name & " test FAILED");
       end if;
    end Run_LCD_Test;
 
