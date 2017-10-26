cf2a393ec17b1277390433974ee2d103e50304b7
Eduard Llamosí <eduard@quri.com>
2017-10-25 19:50:38 -0700

    Amend style

--- core/Gade.ads
+++ core/Gade.ads
@@ -1,8 +1,5 @@
 with Ada.Unchecked_Conversion;
 
-limited with Gade.Input_Reader;
-limited with Gade.Video_Buffer;
-
 package Gade is
 
 private
@@ -15,15 +12,15 @@ private
    type Byte is mod 2 ** 8;
    for Byte'Size use Byte_Size;
 
-   type Signed_Byte is range -128..127;
+   type Signed_Byte is range -128 .. 127;
    for Signed_Byte'Size use Byte_Size;
 
    type Word is mod 2 ** 16;
    for Word'Size use Word_Size;
 
-   -- To use the target machine's native integer type (no representation
-   -- clause is used). Guarantees that the architecture supports at least
-   -- 17 bits which are used for fast carry checks on 16 bit operations.
+   --  To use the target machine's native integer type (no representation
+   --  clause is used). Guarantees that the architecture supports at least
+   --  17 bits which are used for fast carry checks on 16 bit operations.
    type Native_Unsigned is mod 2 ** 17;
 
    function To_Signed is
