b46c3bbac3930fb66a9d7c8110c33853a5a9d19c
Eduard <ellamosi@users.noreply.github.com>
2017-10-29 09:26:51 -0700

    Treat warnings as errors (#5)
    * Treat warnings as errors
    
    * Fix test case style

--- testsuite/tests/instr_timing/src/tc_instr_timing.adb
+++ testsuite/tests/instr_timing/src/tc_instr_timing.adb
@@ -2,5 +2,5 @@ with Test_LCD_Output; use Test_LCD_Output;
 
 procedure TC_Instr_Timing is
 begin
-   Run_LCD_Test("instr_timing.gb", "Instruction timing", 65);
+   Run_LCD_Test ("instr_timing.gb", "Instruction timing", 65);
 end TC_Instr_Timing;
