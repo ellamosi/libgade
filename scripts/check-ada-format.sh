5dc7d66606c37a700414331d6943141476fa1642
ellamosi <ellamosi@users.noreply.github.com>
2026-03-20 09:44:10 -0700

    fix: Don't check format for dependencies

--- scripts/check-ada-format.sh
+++ scripts/check-ada-format.sh
@@ -5,6 +5,6 @@ set -eu
 repo_root=$(CDPATH= cd -- "$(dirname -- "$0")/.." && pwd)
 cd "$repo_root"
 
-gnatformat -P gade.gpr -U --check
-gnatformat -P gade_cpp.gpr -U --check
-alr -C tests exec -- gnatformat -P harness/gade_testd.gpr -U --check
+gnatformat -P gade.gpr --check
+gnatformat -P gade_cpp.gpr --check
+alr -C tests exec -- gnatformat -P harness/gade_testd.gpr --check
--- scripts/format-ada.sh
+++ scripts/format-ada.sh
@@ -5,6 +5,6 @@ set -eu
 repo_root=$(CDPATH= cd -- "$(dirname -- "$0")/.." && pwd)
 cd "$repo_root"
 
-gnatformat -P gade.gpr -U
-gnatformat -P gade_cpp.gpr -U
-alr -C tests exec -- gnatformat -P harness/gade_testd.gpr -U
+gnatformat -P gade.gpr
+gnatformat -P gade_cpp.gpr
+alr -C tests exec -- gnatformat -P harness/gade_testd.gpr
