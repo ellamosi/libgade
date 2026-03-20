5d47b31133de9538403c3ab94e4558b5298f589f
ellamosi <ellamosi@users.noreply.github.com>
2026-03-19 22:56:07 -0700

    style: drop package prefixes, compact

--- core/cpu/gade-dev-cpu-instructions-control.adb
+++ core/cpu/gade-dev-cpu-instructions-control.adb
@@ -1,44 +1,38 @@
 package body Gade.Dev.CPU.Instructions.Control is
 
-   procedure NOP
-     (GB : in out Gade.GB.GB_Type) is
+   procedure NOP (GB : in out GB_Type) is
       pragma Unreferenced (GB);
    begin
       null;
    end NOP;
 
-   procedure CPL
-     (GB : in out Gade.GB.GB_Type) is
+   procedure CPL (GB : in out GB_Type) is
    begin
       GB.CPU.Regs.A := not GB.CPU.Regs.A;
       Set (GB.CPU.Regs.F.H);
       Set (GB.CPU.Regs.F.N);
    end CPL;
 
-   procedure SCF
-     (GB : in out Gade.GB.GB_Type) is
+   procedure SCF (GB : in out GB_Type) is
    begin
       Reset (GB.CPU.Regs.F.N);
       Reset (GB.CPU.Regs.F.H);
       Set (GB.CPU.Regs.F.C);
    end SCF;
 
-   procedure CCF
-     (GB : in out Gade.GB.GB_Type) is
+   procedure CCF (GB : in out GB_Type) is
    begin
       Reset (GB.CPU.Regs.F.N);
       Reset (GB.CPU.Regs.F.H);
       Set_Value (GB.CPU.Regs.F.C, not Is_Set (GB.CPU.Regs.F.C));
    end CCF;
 
-   procedure HALT
-     (GB : in out Gade.GB.GB_Type) is
+   procedure HALT (GB : in out GB_Type) is
    begin
       GB.CPU.Halted := True;
    end HALT;
 
-   procedure STOP
-     (GB : in out Gade.GB.GB_Type) is
+   procedure STOP (GB : in out GB_Type) is
    begin
       GB.CPU.PC := GB.CPU.PC + 1;
       if GB.CPU.IFF = IE_EI then
@@ -46,14 +40,12 @@ package body Gade.Dev.CPU.Instructions.Control is
       end if;
    end STOP;
 
-   procedure DI
-     (GB : in out Gade.GB.GB_Type) is
+   procedure DI (GB : in out GB_Type) is
    begin
       GB.CPU.IFF := IE_DI;
    end DI;
 
-   procedure EI
-     (GB : in out Gade.GB.GB_Type) is
+   procedure EI (GB : in out GB_Type) is
    begin
       GB.CPU.IFF := IE_EI;
    end EI;
