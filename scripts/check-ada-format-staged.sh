428a9a80e870847c9e5af7b80970384985251f90
ellamosi <ellamosi@users.noreply.github.com>
2026-03-20 19:44:51 -0700

    style: Have format check be project settings aware

--- scripts/check-ada-format-staged.sh
+++ scripts/check-ada-format-staged.sh
@@ -20,12 +20,17 @@ for path in "$@"; do
   case "$path" in
     tests/harness/*)
       rel_path=${path#tests/}
-      if ! alr -C tests exec -- gnatformat --check "$rel_path"; then
+      if ! alr -C tests exec -- gnatformat -P harness/gade_testd.gpr --no-subprojects --check "$rel_path"; then
+        status=1
+      fi
+      ;;
+    cpp/*)
+      if ! gnatformat -P gade_cpp.gpr --no-subprojects --check "$path"; then
         status=1
       fi
       ;;
     *)
-      if ! gnatformat --check "$path"; then
+      if ! gnatformat -P gade.gpr --no-subprojects --check "$path"; then
         status=1
       fi
       ;;
